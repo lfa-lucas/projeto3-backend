4328d7863ca27dd3551a7cd0c44f07fb9c55864a
Yuri Brito <yurisb85@gmail.com>
2022-12-12 19:05:40 -0300

    Alterações para troca de email na page profile

--- model/user.model.js
+++ model/user.model.js
@@ -1,46 +1,51 @@
 import { model, Schema } from "mongoose";
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/gm,
-  },
-  passwordHash: {
-    type: String,
-    required: true,
-  },
-  active: {
-    type: Boolean,
-    default: true,
-  },
-  role: {
-    type: String,
-    enum: ["usuario", "gestor", "admin"],
-    default: "usuario",
-  },
-  setor: {
-    type: Schema.Types.ObjectId,
-    ref: "Setor",
-  },
-  profileImg: {
-    type: String,
-  },
-  tarefas: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Tarefa",
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
     },
-  ],
-});
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      match: /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/gm,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    active: {
+      type: Boolean,
+      default: true,
+    },
+    role: {
+      type: String,
+      enum: ["usuario", "gestor", "admin"],
+      default: "usuario",
+    },
+    setor: {
+      type: Schema.Types.ObjectId,
+      ref: "Setor",
+    },
+    profileImg: {
+      type: String,
+    },
+    tarefas: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Tarefa",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const UserModel = model("User", userSchema);
 
